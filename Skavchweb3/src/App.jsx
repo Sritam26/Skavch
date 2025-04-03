5df1be5bfa9ec9fc8e3941c7b1a0875159beafac
Kirti das <128667028+kirti0229@users.noreply.github.com>
2025-04-03 11:52:54 +0530

    TASK011:OurLeadership

--- Skavchweb3/src/App.jsx
+++ Skavchweb3/src/App.jsx
@@ -1,11 +1,11 @@
-
-
 import './App.css'
-
+import {OurLeadership} from "./Components/OurLeadership/OurLeadership"
+import {WorkTogether} from "./Components/WorkTogether/WorkTogether"
 function App() {
   return (
     <>
-      
+        <OurLeadership />
+        <WorkTogether />
     </>
   )
 }
